287bc9e7eb7ea1d1e088deeb8b3da58aaeeaf2fb
enricomeloni <enrico-meloni@hotmail.it>
2018-11-15 23:31:26 +0100

    Add Migrations for EF

---
+++ MemeAudioBot/MemeAudioBot/Migrations/20181115220119_InitialCreate.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MemeAudioBot.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Audios",
+                columns: table => new
+                {
+                    AudioId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true),
+                    Url = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Audios", x => x.AudioId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Audios");
+        }
+    }
+}
