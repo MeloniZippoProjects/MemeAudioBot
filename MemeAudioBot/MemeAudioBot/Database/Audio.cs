c8c8d7adecb5660bd40ce162c5f165fc47b86c8a
enricomeloni <enrico-meloni@hotmail.it>
2018-11-15 23:31:16 +0100

    Create DB models

---
+++ MemeAudioBot/MemeAudioBot/Database/Audio.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MemeAudioBot.Database
+{
+    public class Audio
+    {
+        public int AudioId { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"{AudioId}: {Name}";
+        }
+    }
+}
---
+++ MemeAudioBot/MemeAudioBot/Database/AudioContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace MemeAudioBot.Database
+{
+    public class AudioContext : DbContext
+    {
+        public static string ConnectionString;
+        public AudioContext(DbContextOptions<AudioContext> options) : base(options)
+        { }
+
+        public DbSet<Audio> Audios { get; set; }
+    }
+}
