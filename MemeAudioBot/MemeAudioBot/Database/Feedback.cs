9dd74671ea646cee1c632dcc1a5f3f75da86e4e5
enricomeloni <enrico-meloni@hotmail.it>
2018-11-19 14:47:50 +0100

    Add Feedback database table

---
+++ MemeAudioBot/MemeAudioBot/Database/Feedback.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MemeAudioBot.Database
+{
+    public class Feedback
+    {
+        public int FeedbackId;
+        public string Text;
+        public string MessageJson;
+    }
+}
